06fc0856f4e6b00f3495104ea374430c97b6dbca
Sarthak <sarthakiet2019@gmail.com>
2023-07-31 00:01:37 +0530

    beta app running

--- app/src/main/java/io/realworld/quotes_app/MainViewModel.kt
+++ app/src/main/java/io/realworld/quotes_app/MainViewModel.kt
@@ -1,8 +1,35 @@
 package io.realworld.quotes_app
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
 
-class MainViewModel: ViewModel() {
+class MainViewModel(val context: Context): ViewModel() {
+
+    private var quotelist: Array<Quote> = emptyArray()
+    private var index = 0
+
+    init{
+        quotelist = loadQuotesfromAssets()
+    }
+
+    private fun loadQuotesfromAssets() : Array<Quote>{
+
+        val inputString = context.assets.open("quotes.json")
+        val size: Int = inputString.available()
+        val buffer = ByteArray(size)
+        inputString.read(buffer)
+        inputString.close()
+        val json = String(buffer,Charsets.UTF_8)
+        val gson = Gson()
+        return gson.fromJson(json, Array<Quote>::class.java)
+
+    }
+    fun getQuote() = quotelist[index]
+
+    fun nextQuote() = quotelist[++index]
+
+    fun previousQuote() = quotelist[--index]
 
 
 }
